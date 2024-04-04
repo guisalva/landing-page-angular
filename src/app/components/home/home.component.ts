7812e18bc281dc7a4728cd6dc5dee7e4a3436a49
Guilherme Salvador <guilherme@ti18.COBRECOM>
2024-04-04 13:57:13 -0300

    Continuação do newsletter e benefits

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../header/header.component';
+import { BtnPrimaryComponent } from '../btn-primary/btn-primary.component';
 import { NgOptimizedImage } from '@angular/common';
+import { NewsletterFormComponent } from '../newsletter-form/newsletter-form.component';
+import { BenefitComponent } from '../benefit/benefit.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [
     HeaderComponent,
+    BtnPrimaryComponent,
+    NewsletterFormComponent,
+    BenefitComponent,
     NgOptimizedImage
   ],
   providers: [],
